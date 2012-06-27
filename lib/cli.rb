14c50d0d280910ea27890b659ae699be32f41295
Nikhil Bhardwaj <root@NikhilBhardwaj.in>
2012-06-27 21:19:56 +0530

    Creates the directory for our gem

---
+++ lib/cli.rb
@@ -0,0 +1,12 @@
+# This file handles the tasks that are to be performed when chagol is installed
+# It involves creating the .chagol directory in the users home and also 
+# initializing the json files that store the contents
+
+module Chagol
+  def install_chagol
+    chagol_path = Dir.home + ".chagol"
+    Dir.mkdir(chagol_path)
+    # Create the sample json files for the contacts
+    # Create the json for the lists
+  end
+end
